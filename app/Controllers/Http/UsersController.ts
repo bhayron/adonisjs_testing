09454b32fea4096192cbe6803e2cc8aea562eabe
Bhayron <bhayronklivilan@gmail.com>
2023-07-17 11:38:14 -0300

    finish

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -5,13 +5,11 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async index({ response }: HttpContextContract) {
     const users = await User.query().preload('profile')
-
     return response.ok(users)
   }
   public async store({ request, response }: HttpContextContract) {
     const data = request.only(['name', 'email', 'password'])
     const user = await User.create(data)
-
     return response.created({ user })
   }
   public async update({ request, params }: HttpContextContract) {
@@ -29,20 +27,15 @@ export default class UsersController {
   }
   public async profileRegister({ request, response }: HttpContextContract) {
     const data = request.only(['userId', 'fullName', 'avatarUrl'])
-
     const profile = await Profile.create(data)
-
     return response.created({ profile })
   }
   public async login({ request, auth, response }: HttpContextContract) {
     const email = request.input('email')
     const password = request.input('password')
-
     const user = await User.query().where('email', email).first()
-
     if (user) {
       const token = await auth.use('api').attempt(email, password)
-
       return response.status(200).json({ token: token.token, user: user.serialize() })
     } else {
       return response.unauthorized({ message: 'Credenciais inválidas' })
